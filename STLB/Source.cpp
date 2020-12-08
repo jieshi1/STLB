6402895832e9cb0b09d4c4ade095b04bb939ce7b
Jie Shi <jie.shi@sinclair.edu>
2020-12-08 13:10:15 -0500

    Add project files.

---
+++ STLB/Source.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+template <class T>
+T half(T num) {
+	T result;
+	result = num / 2;
+	return result;
+}
+
+int half(int num) {
+    int result;
+    result = round(static_cast<double>(num) / 2);
+    return result;
+	
+}
+
+int main() {
+    double a = 7.0;
+    float b = 5.0f;
+    int c = 3;
+
+    cout << half(a) << endl;
+    cout << half(b) << endl;
+    cout << half(c) << endl;
+}
